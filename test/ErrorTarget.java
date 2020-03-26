45b36d3f2f71d696ce4981dc642e83b5dbf3db2c
Mgr. Vladimir Magyar <magyarvladimir@gmail.com>
2020-03-26 14:42:25 +0100

    Completed go porting

--- test/ErrorTarget.java
+++
@@ -1,9 +0,0 @@
-package com.ocrsdk.abbyy.v2.client;
-
-public class ErrorTarget {
-    public static final String File = "File";
-    public static final String TaskId = "TaskId";
-    public static final String Language = "Language";
-    public static final String Description = "Description";
-    public static final String Region = "Region";
-}
